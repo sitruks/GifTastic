6908c22c070ce60e313c2e85e7203bf9a448bdec
SITRUK <kurtis.santillanes@gmail.com>
2019-04-07 16:56:24 -0700

    fixed button population error and local memory bug

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,140 +1,96 @@
-// //// 
-
 var gifs =
-{ 
-  queue: 
-  [
-    {id: 0, name:"Bugs Bunny"},
-    {id: 1, name:"Daffy Duck"},
-    {id: 2, name:"Porky Pig"},
-    {id: 3, name:"Marvin the Martian"},
-    {id: 4, name:"Foghorn Leghorn"},
-    {id: 5, name:"Wile E. Coyote"},
-    {id: 6, name:"The Roadrunner"}
-  ]
+{
+    queue:
+        [
+            { id: 0, name: "Bugs Bunny" },
+            { id: 1, name: "Daffy Duck" },
+            { id: 2, name: "Porky Pig" },
+            { id: 3, name: "Marvin the Martian" },
+            { id: 4, name: "Foghorn Leghorn" },
+            { id: 5, name: "Wile E Coyote" },
+            { id: 6, name: "The Roadrunner" }
+        ]
 };
-
 localStorage.setItem('gifs', JSON.stringify(gifs));
-
 var itemCount = 0;
-$("#newButton").on("click", function (event) {
-    // BEST PRACTICE TO PREVENT FORM FROM SUBMITTING
+
+$("#newGifButton").on("click", function (event) {
+    // PREVENT FORM FROM SUBMITTING
     event.preventDefault();
-    
-    // GET THE INPUT VALUE FROM THE USER TO BE ADDED TO A BUTTON
-    var addGifText = $('#nameField').val();
-    
-    createItem(itemCount, addGifText);
+    var newGifTextInput = $('#newGifText').val();
+    createItem(itemCount, newGifTextInput);
     itemCount++;
-    
 });
 
-function createItem(itemNumber, addGifText) {
+
+var restoredGifs = JSON.parse(localStorage.getItem('gifs'));
+console.log(restoredGifs);
+var outputs = "";
+function createItem(itemNumber, newGifTextInput) {
+    var gifButton;
     var itemId = "item-" + itemNumber;
-    
-    var todoP = $('<p>').attr("id", itemId).text(addGifText);
-    
-    localStorage.setItem(itemId, addGifText);
-    var itemBtn = $("<button>", {
-        "gifAdded": itemNumber,
+    // var gifContent = $('<li class="list-group-item">').attr("id", itemId).text(newGifTextInput);
+    var gifContent = $(`<li id="${itemId}" class="list-group-item"><input type="button" id="gifCallButton" class="nes-btn" 
+    value="${newGifTextInput}"/></div></li>`);
+    localStorage.setItem(itemId, newGifTextInput);
+    var removeGif = $("<button>", {
+        "gifToRemove": itemNumber,
         class: "checkbox",
         text: 'X'
     });
-    
-    // APPEND THE BUTTON TO THE newButton ITEM
-    todoP.prepend(itemBtn);
-    $('#resultsButton').append(todoP);
-    
-    // THEN CLEAR THE TEXTBOX WHEN DONE
-    $('#nameField').val("");
-}
+
+    // Append the button to the to do item
+    gifButton = gifContent.prepend(removeGif);
+
+    $('#resultsButton').append(gifButton);
+
+    // Clear the textbox when done
+    $('#newGifText').val("");
+};
 
 $(document.body).on("click", ".checkbox", function () {
-    
-    var itemId = $(this).attr("gifAdded");
+    var itemId = $(this).attr("gifToRemove");
     console.log(itemId);
     $('#item-' + itemId).empty();
-    
 });
 
 $(document).ready(function () {
-    
-    makeGif();
 
-    function makeGif() {
-      var restoredGifs = JSON.parse(localStorage.getItem('gifs'));
-      var outputs = "";
-      for(var i = 0; i < restoredGifs.queue.length; i++)
-      {
-          outputs += `<div id="${restoredGifs.queue[i].id}">
-          <input type="button" id="gifCallButton" class="nes-btn" 
-          value="${restoredGifs.queue[i].name}"/></div>`;
-      }
-      document.getElementById("resultsButton").innerHTML= outputs;
+    for (var i = 0; i < restoredGifs.queue.length; i++) {
+        var gifName = restoredGifs.queue[i].name
+        console.log(gifName)
+        // var newGifTextInput = $(gifName).val();
+        createItem(itemCount, gifName);
+        itemCount++;
     }
-    function addGif() {
-      var restoredGifs = JSON.parse(localStorage.getItem('gifs'));
-      
-      restoredGifs.queue.push({
-        id:  Math.floor(Math.random() * (100 - 1 + 1)) + 1,
-        name: $('input').val()
-      });
-      localStorage.setItem('gifs', JSON.stringify(restoredGifs));
-      makeGif();
-    }
-    
 });
 
-//// 
-//// 
+// $('#gifCallButton').on('click', function (event) {
+//     // BEST PRACTICE TO PREVENT FORM FROM SUBMITTING
+//     event.preventDefault();
+//     //// CALL AND POPULATE GIF
+//     function getGif() {
+//         // GET
+//         var magic;
+//         console.log(magic);
+//         magic = $('#gifCallButton').val();
+//         //SET
+//         $('#gifCallButton').val(magic);
+//         console.log(magic);
+//         var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=zGfrjOHn6S3dvmRKo9mb00sGQ729qbEd&tag=" + magic + "";
+//         $.ajax({
+//             url: queryURL,
+//             method: "GET"
+//         })
+//             .then(function (response) {
+//                 console.log(response);
+//                 var imageUrl = response.data.image_original_url;
+//                 var magicImage = $("<img>");
+//                 magicImage.attr("src", imageUrl);
+//                 magicImage.attr("alt", "magic image");
+//                 $("#resultsImage").prepend(magicImage);
+//             });
+//     };
+//     getGif();
 
-CALL AND POPULATE GIF
-function getGif() {
-
-    // GET
-    var magic;
-    console.log(magic);
-    magic = $('#nameField').val();
-    //SET
-    $('#nameField').val(magic);
-    console.log(magic);
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=zGfrjOHn6S3dvmRKo9mb00sGQ729qbEd&tag=" + magic + "";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function (response) {
-            console.log(response);
-            var imageUrl = response.data.image_original_url;
-            var magicImage = $("<img>");
-            magicImage.attr("src", imageUrl);
-            magicImage.attr("alt", "magic image");
-            $("#resultsImage").prepend(magicImage);
-        });
-};
-
-// MAKE BUTTONS TO CALL GIF
-function getGifButton() {
-
-    // GET
-    var magic;
-    magic = $('#nameField').val();
-    //SET
-    $('#nameField').val(magic);
-    console.log(magic);
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=zGfrjOHn6S3dvmRKo9mb00sGQ729qbEd&tag=" + magic + "";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function (response) {
-            console.log(response);
-            var imageUrl = response.data.image_original_url;
-            var magicImage = $("<img>");
-            magicImage.attr("src", imageUrl);
-            magicImage.attr("alt", "magic image");
-            $("#resultsImage").prepend(magicImage);
-        });
-};
+// });
